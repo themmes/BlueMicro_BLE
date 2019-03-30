42b8aa7e5e7623b671b4116a53210d1ea247e774
Julian Komaromy <julian.komaromy@gmail.con>
2019-03-30 09:52:21 +0100

    update keypress to single

--- firmware/Keypress.h
+++ firmware/Keypress.h
@@ -5,38 +5,36 @@
 #ifndef KEYPRESS_H
 #define KEYPRESS_H
 
-using Modifier = std::vector<std::pair<bool, uint32_t>>;
+using Method = std::vector<std::pair<bool, uint32_t>>;
 using Activations = std::array<Modifier, 3>;
 
 class Keypress 
 {
     public: 
         Keypress();
-        Keypress(Activations acts); 
+        Keypress(Method m); 
 
-        void clear(unsigned long currentMillis);
-        void press(unsigned long currentMillis);
+        void clear(unsigned long delta, bool wasPress);
+        void press(unsigned long delta, bool wasPress);
 
-        //unused
-        std::array<bool, 3> read();
-
-        std::size_t singleRead();
-
+        bool isActive();
     private:
-        void checkActivations(unsigned long currentMillis, bool press);
+        void checkActivations(unsigned long delta, bool wasPress, bool press);
 
-        bool extrema(uint32_t delta, 
-                std::pair<bool, uint32_t> current, 
+        bool extrema(unsigned long delta, 
+                std::pair<bool, uint8_t> current, 
+                bool wasPress,
                 bool press, 
                 bool even);
 
-        void updateCandidate(std::size_t i);
+        void updateIndex();
+        //void updateLastOn(unsigned long currentMillis, bool press);
 
-        void updateLastOn(unsigned long currentMillis, bool press);
+        Method method;
 
-        Activations activations;
         bool wasPress;
-        unsigned long lastChange;
-        std::array<std::size_t, 3> candidates;
+        //unsigned long lastChange;
+
+        std::size_t index;
 };
 #endif 
