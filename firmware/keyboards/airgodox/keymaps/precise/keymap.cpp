a6de0a55b5e867c28d29a850d21d400bf4acc9de
Julian Komaromy <julian.komaromy@gmail.con>
2019-04-15 16:55:30 +0200

    update agdx config

--- firmware/keyboards/airgodox/keymaps/precise/keymap.cpp
+++ firmware/keyboards/airgodox/keymaps/precise/keymap.cpp
@@ -17,13 +17,18 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 
 #include "keymap.h"
 
-#if KEYBOARD_SIDE == LEFT
+#if KEYBOARD_SIDE == SINGLE
 //TODO: Keymap as namespace to hide these globals?
 
 //a keypress that activates upon press, but is only
 //reset when it is released
 Keypress toggle { {{ {false, 1}, {false, 1} }}, 1 };
+Keypress hold { {{ {false, 20} }} };
+
 VKey TG_SHIFT {toggle, TG(KC_LSHIFT)};
+VKey TG_L1 {toggle, TG(LAYER_1)};
+VKey OS_SHIFT {OS(KC_LSHIFT)}
+VKey HOLD_W {hold, KC_W};
 
 /*
  * initiialize the default layer (QWERTY/PRESS) with the following
@@ -31,10 +36,10 @@ VKey TG_SHIFT {toggle, TG(KC_LSHIFT)};
  */
 layer_t layer0 
 {{
-    {KC_ESC,    KC_Q,          KC_W,  KC_E,        KC_R,     KC_T},
-        {KC_TAB,    KC_NO,    KC_S,  KC_CAP_D,    KC_F,     KC_G},
-        {KC_LSHIFT, TG_SHIFT, KC_X,  KC_C,        KC_V,     KC_B},
-        {KC_NO,     KC_NO,    KC_NO, TG(LAYER_1), KC_E, KC_LGUI}
+    {KC_ESC,    OS_SHIFT,          HOLD_W,  KC_E,    KC_R,     KC_T},
+        {KC_TAB,    KC_NO,    KC_S,  KC_CAP_D, KC_F,     KC_G},
+        {KC_LSHIFT, TG_SHIFT, KC_X,  KC_C,     KC_V,     KC_B},
+        {KC_NO,     KC_NO,    KC_NO, TG_1,     KC_E,  KC_LGUI}
  }};
 
 layer_t layer1 
@@ -42,7 +47,7 @@ layer_t layer1
     {KC_GRV,    KC_1,    KC_2,     KC_3,    KC_4,     KC_5},
         {KC_CAPS,   KC_F1,   KC_F2,    KC_F3,   KC_F4,    KC_F5}, 
         {KC_LSHIFT, KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10}, 
-        {KC_NO,     KC_NO,   KC_NO,    TG(LAYER_1), KC_E, KC_LGUI}
+        {KC_NO,     KC_NO,   KC_NO,    TG_L1,   KC_E,     KC_LGUI}
  }};
 
 matrix_t matrix = { layer0, layer1};
